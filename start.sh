63b09ab861310ea8dec3fc05bda677e9f3c857a6
Herve Nicol <herve.nicol@gmail.com>
2015-07-19 16:00:32 +0200

    start.sh: improved verbosity and error management

--- start.sh
+++ start.sh
@@ -8,20 +8,29 @@
 IMAGE_NAME="hervenicol/influxdb"
 IMAGE_TAG="0.8.8"
 CONT_NAME="influxdb"
-PORT_PROTOBUF="8099"
-PORT_RAFT="8090"
+#PORT_PROTOBUF="8099"
+#PORT_RAFT="8090"
 PORT_API="8086"
 PORT_ADMIN="8083"
 PORT_COLLECTD=25826
 #### End Configuration ####
 
+
+echo "Starting Docker container $CONT_NAME"
 docker run -d -i -t \
 	-p "$PORT_API":8086 \
 	-p "$PORT_ADMIN":8083 \
 	-p "$PORT_COLLECTD":25826/udp \
 	--name "$CONT_NAME" \
-	"$IMAGE_NAME":"$IMAGE_TAG" && \
-wget -O- -w 5 "http://localhost:$PORT_API/db/site_dev/users?u=root&p=root" &&
+	"$IMAGE_NAME":"$IMAGE_TAG" &&
+
+echo "Container $CONT_NAME started; now waiting for service to be up"  &&
+wget -q -O/dev/null -w 5 "http://localhost:$PORT_API/db/site_dev/users?u=root&p=root" &&
+
+echo "Container $CONT_NAME up and running; now creating collectd database" &&
 sleep 2 &&
-wget -O- -w 5 "http://localhost:$PORT_API/db?u=root&p=root" --post-data '{"name": "collectd"}'
+wget -q -O- -w 5 "http://localhost:$PORT_API/db?u=root&p=root" --post-data '{"name": "collectd"}' &&
+
+echo "$CONT_NAME ready to use!" ||
+echo "$CONT_NAME failed starting up!"
 
