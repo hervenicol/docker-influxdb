c328844ce1f35ab4b1648834990f7e62fe2ac350
Herve Nicol <herve.nicol@gmail.com>
2015-08-06 20:49:19 +0200

    switch to influxdb 0.9 - updated image tag, and removed db creation, not needed anymore

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@
 
 #### Configuration ####
 IMAGE_NAME="hervenicol/influxdb"
-IMAGE_TAG="0.8.8"
+IMAGE_TAG="0.9.2"
 CONT_NAME="influxdb"
 #PORT_PROTOBUF="8099"
 #PORT_RAFT="8090"
@@ -22,15 +22,5 @@ docker run -d -i -t \
 	-p "$PORT_ADMIN":8083 \
 	-p "$PORT_COLLECTD":25826/udp \
 	--name "$CONT_NAME" \
-	"$IMAGE_NAME":"$IMAGE_TAG" &&
-
-echo "Container $CONT_NAME started; now waiting for service to be up"  &&
-wget -q -O/dev/null -w 5 "http://localhost:$PORT_API/db/site_dev/users?u=root&p=root" &&
-
-echo "Container $CONT_NAME up and running; now creating collectd database" &&
-sleep 2 &&
-wget -q -O- -w 5 "http://localhost:$PORT_API/db?u=root&p=root" --post-data '{"name": "collectd"}' &&
-
-echo "$CONT_NAME ready to use!" ||
-echo "$CONT_NAME failed starting up!"
+	"$IMAGE_NAME":"$IMAGE_TAG"
 
